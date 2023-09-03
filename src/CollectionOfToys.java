070614f8222c457ea88d974cf854690daf3d687f
DenitKO <motovdenis@mail.ru>
2023-09-03 20:12:39 +0300

    Some changes, not done yet

--- src/CollectionOfToys.java
+++ src/CollectionOfToys.java
@@ -1,5 +1,57 @@
+import java.awt.*;
+import java.util.ArrayList;
 import java.util.PriorityQueue;
-public class CollectionOfToys extends PriorityQueue {
+import java.util.Random;
+import java.util.stream.IntStream;
 
+public class CollectionOfToys {
+    private static int countOfToys;
+    private ArrayList listOfToys;
+    private static int length;
 
+    {
+        length +=1;
+    }
+
+    public void CollectionOfToys()
+    {
+
+    }
+
+    public void add (Toy toy){
+        this.listOfToys.add(toy);
+        this.countOfToys += toy.getCount();
+    }
+    public char DrawingOfPrizes(char[] values, int[] weights)
+    {
+        int total = IntStream.of(weights).sum();
+        int n = 0;
+        char myFinish = '0';
+
+        int num = new Random().nextInt(total- 1)+1;
+        System.out.println(num);
+
+        for (int i = 0; i < values.length; i++) {
+            n += weights[i];
+            if ( n >= num )
+            {
+                return values[i];
+            }
+        }
+        return myFinish;
+    }
+
+    public static int getLenth() {
+        return length;
+    }
+
+    public void clear(){
+        this.listOfToys.clear();
+        this.length = 0;
+    }
+
+    public void remove (Toy toy){
+        this.listOfToys.remove(toy);
+        this.length -= 1;
+    }
 }
--- src/Toy.java
+++ src/Toy.java
@@ -1,21 +1,30 @@
 public class Toy implements Comparable<Toy>{
     private static int id_counter = 0;
-    private int id;
-    private String name;
+    private final int id;
+    private int count;
+    private final String name;
     private int randomRatio;
 
     {
         this.id = id_counter;
-        id_counter +=1;
+        this.id_counter +=1;
+    }
+
+    public Toy(int randomRatio, int count, String name){
+        setRandomRatio(randomRatio);
+        setCount(count);
+        this.name = name;
     }
 
     public Toy(int randomRatio, String name){
-        this.randomRatio = randomRatio;
+        setRandomRatio(randomRatio);
+        this.count = 1;
         this.name = name;
     }
 
     public Toy(String name){
         this.randomRatio = 1;
+        this.count = 1;
         this.name = name;
     }
 
@@ -27,12 +36,21 @@ public class Toy implements Comparable<Toy>{
         return this.name;
     }
 
-    public int getRandomRatio() {
-        return this.randomRatio;
+    public int getCount() { return count; }
+
+    public void setCount(int count) {
+        if (count > 0){
+            this.count = count;
+        }
+        else System.out.println("Введите количество игрушек");
     }
 
+    public int getRandomRatio() { return this.randomRatio; }
+
     public void setRandomRatio(int randomRatio){
-        this.randomRatio = randomRatio;
+        if (randomRatio > 0 || randomRatio <= 100)
+            this.randomRatio = randomRatio;
+        else System.out.println("Ошибка. Введите число от 1 до 100");
     }
 
     @Override
